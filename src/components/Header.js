3d34f810388586fd88337260e008e6c81bb63f7e
Shivang <rawatofficial2021@gmail.com>
2023-02-25 22:49:38 +0530

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+// import { render } from '@testing-library/react'
+import './App.css';
+import React from 'react'
+
+export const Header = () => {
+
+  return (
+
+    <center><div className="container">
+        {/* <h1 style={{color : "red"}}>Hello world!</h1> */}
+        <img src="../images/1.jpg" alt="" srcset="" />
+
+    </div></center>
+  )
+}
